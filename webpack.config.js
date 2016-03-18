0c125e538bd08f931406b4f0de88474b2624e1c3
Bryant Syme <bsyme@jamasoftware.com>
2016-03-18 16:46:00 -0700

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
         test: /\.(js|jsx)$/,
         loaders: ['react-hot', 'babel'],
         exclude: /relationship-viewer\/node_modules/
+    }, {
+        test: /\.css$/,
+        loaders: ['style', 'css?modules&localIdentName=[path][name]---[local]---[hash:base64:5]'],
+        exclude: /relationship-viewer\/node_modules/
     }]
   }
 };
